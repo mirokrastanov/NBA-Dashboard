826adc3de9c1558ab74cb908e1794b8838581789
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 12:01:41 +0300

    Separate structure into autonomous components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
-import { Order, orders } from '../assets/mockData';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -7,53 +6,6 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  constructor(@Inject(DOCUMENT) private document: Document) { }
-
-  orders: Order[] = orders;
-  intervals: any[] = [];
-
-  ngOnInit(): void {
-    // console.log(window.innerWidth + 'x' + window.innerHeight);
-    let aside = () => document.body.querySelector('aside')!;
-    let interval = setInterval(() => {
-      if (window.innerWidth > 768) {
-        if (aside().style.display = 'none') {
-          aside().style.display = 'block';
-        }
-      }
-    }, 100);
-    this.intervals.push(interval);
-  }
-
-  ngOnDestroy(): void {
-    this.intervals.forEach(timer => clearInterval(timer));
-  }
-
-  menuHandler(menuEl: HTMLElement): void {
-    menuEl.style.display = 'block';
-  }
-
-  closeHandler(menuEl: HTMLElement): void {
-    menuEl.style.display = 'none';
-  }
-
-  searchHandler(e: MouseEvent, sForm: HTMLElement, sBtn: HTMLElement, sIcon: HTMLElement): void {
-
-    if (window.innerWidth <= 768) {
-      e.preventDefault();
-      sForm.classList.toggle('show');
-      if (sForm.classList.contains('show')) {
-        sIcon.textContent = 'close';
-      } else {
-        sIcon.textContent = 'search';
-      }
-    }
-  }
-
-  modeSwitchHandler(light: HTMLElement, dark: HTMLElement): void {
-    this.document.body.classList.toggle('dark-mode-variables');
-    light.classList.toggle('active');
-    dark.classList.toggle('active');
-  }
+export class AppComponent {
+  
 }
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedModule } from '../shared/shared.module';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    SidebarComponent,
+    TopNavComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [
-
+    DashboardComponent,
+    SidebarComponent,
+    TopNavComponent,
   ],
 })
 export class CoreModule { }
