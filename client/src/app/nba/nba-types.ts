b6e01124154d2194765c28ad6589f944cc0986c2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 18:39:44 +0300

    Add Player and Team interfaces

---
+++ client/src/app/nba/nba-types.ts
@@ -0,0 +1,20 @@
+export interface Team {
+    id: number;
+    abbreviation: string;
+    city: string;
+    conference: string;
+    division: string;
+    name: string;
+    full_name: string;
+}
+
+export interface Player {
+    id: number;
+    first_name: string;
+    last_name: string;
+    position: string;
+    height_feet: null | number;
+    height_inches: null | number;
+    weight_pounds: null | number;
+    team: Team;
+}
