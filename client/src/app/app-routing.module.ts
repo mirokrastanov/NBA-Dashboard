b171a625f6c56b52ff47f5abf2192aabaa3efa78
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 17:02:05 +0300

    Add new route + Auto hide nav @click on mobile dev

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Inject } from '@angular/core';
+import { NavigationSkipped, NavigationStart, Router, RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { PlayersComponent } from './nba/players/players.component';
+import { DOCUMENT } from '@angular/common';
 
 
 const routes: Routes = [ // UPPER/LOWER CASE MATTERS !!!!!!! routerLink in the html should MATCH IT!!!
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'players', component: PlayersComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router, @Inject(DOCUMENT) private document: Document) {
+    router.events.subscribe((event) => {
+      // console.log(event);
+      if (event instanceof NavigationStart || event instanceof NavigationSkipped) {
+        // console.log('path change');
+        if (window.innerWidth <= 768) {
+          this.aside().style.display = 'none';
+        }
+      }
+    });
+  }
+
+  aside = () => this.document.body.querySelector('aside')!;
+
+}
