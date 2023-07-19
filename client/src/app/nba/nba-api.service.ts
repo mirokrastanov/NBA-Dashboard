cbad24c97021ffb58965ed25cf463d3232b60829
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 18:18:58 +0300

    Improve fetching and empty/wrong res handling

--- client/src/app/nba/nba-api.service.ts
+++ client/src/app/nba/nba-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { endpointsNBA, proxy, noProxy } from '../util/global-contants';
+import { EMPTY } from 'rxjs'; // returns an empty observable
 
 
 @Injectable({
@@ -11,8 +12,8 @@ export class NbaApiService {
 
   constructor(private http: HttpClient) { }
 
-  nbaFetch(target: string) { // TODO ==> Add and implement dynamic query params input
-    if (!target) target = endpointsNBA.stats;
+  nbaFetch(target: string = '') { // TODO ==> Add and implement dynamic query params input
+    if (!target || !endpointsNBA.hasOwnProperty(target)) return EMPTY;
     let address = endpointsNBA[target];
     return this.http.get<any>(proxy + address);
   }
--- client/src/app/nba/players/players.component.ts
+++ client/src/app/nba/players/players.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { NbaApiService } from '../nba-api.service';
 
 @Component({
@@ -10,20 +10,28 @@ export class PlayersComponent implements OnInit {
   constructor(private apiService: NbaApiService) { }
   playersArray: any;
   metaObject: any;
+  unsubscribed: boolean = false;
+  errorOccurred: boolean = false;
 
   ngOnInit(): void {
-    this.apiService.nbaFetch('players').subscribe({
+    this.apiService.nbaFetch('play').subscribe({
       next: (data) => {
         this.playersArray = data.data;
         this.metaObject = data.meta;
         console.log(data);
+        console.log(this.metaObject);
         console.log(data.data[0]);
-        let test: object = {};
-        console.log(test.hasOwnProperty('players'));
       },
       error: (err) => {
+        this.errorOccurred = true;
         console.log(err);
-      }
+      },
+      complete: () => {
+        this.unsubscribed = true;
+        console.log('Subscription ended!');
+      },
     });
   }
+
+
 }
