2dde784b6d8cdb0eaf4af66c60913323cd158799
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 12:00:58 +0300

    Add new component

---
+++ client/src/app/core/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject, OnInit, OnDestroy, Output } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit, OnDestroy {
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+  aside = () => this.document.body.querySelector('aside')!;
+
+  intervals: any[] = [];
+
+  ngOnInit(): void {
+    // console.log(window.innerWidth + 'x' + window.innerHeight);
+    let interval = setInterval(() => {
+      if (window.innerWidth > 768) {
+        if (this.aside().style.display = 'none') {
+          this.aside().style.display = 'block';
+        }
+      }
+    }, 100);
+    this.intervals.push(interval);
+  }
+
+  ngOnDestroy(): void {
+    this.intervals.forEach(timer => clearInterval(timer));
+  }
+
+  closeHandler(menuEl: HTMLElement): void {
+    menuEl.style.display = 'none';
+  }
+}
