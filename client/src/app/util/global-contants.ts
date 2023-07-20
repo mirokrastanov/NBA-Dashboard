1bd63bed64ce6e66aeb62392d4b743b9b0fe5c0b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-20 19:43:32 +0300

    Add firebase data endpoint for fetching data

--- client/src/app/util/global-contants.ts
+++ client/src/app/util/global-contants.ts
@@ -3,6 +3,7 @@ export const noProxy: string = 'https://';
 
 export const nbaAPIdomain: string = 'www.balldontlie.io/api/v1';
 
+// API - free data with flaws and not all the data needed
 export const endpointsNBA: any = { // any to avoid property checking errors
     players: nbaAPIdomain + '/players',
     teams: nbaAPIdomain + '/teams',
@@ -11,5 +12,10 @@ export const endpointsNBA: any = { // any to avoid property checking errors
     seasonAvg: nbaAPIdomain + '/season_averages'
 };
 
+// MY Firebase DB - with scraped data - TODO: deploy the scraper online so it updates it on certain interval
+export const firebaseNBA: any = {
+    standings: 'https://nba-1-480a7-default-rtdb.europe-west1.firebasedatabase.app/.json',
 
-// TODO --> add firebase fetching URL
+};
+
+// TODO --> add firebase fetching URL for users + auth all
