e15843f1400bc83490240e13e8d86ee4cef7c52a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-22 11:02:51 +0300

    Add standings component

--- client/src/app/nba/nba.module.ts
+++ client/src/app/nba/nba.module.ts
@@ -4,12 +4,14 @@ import { PlayersComponent } from './players/players.component';
 import { PlayerDetailsComponent } from './player-details/player-details.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { StandingsComponent } from './standings/standings.component';
 
 
 @NgModule({
   declarations: [
     PlayersComponent,
-    PlayerDetailsComponent
+    PlayerDetailsComponent,
+    StandingsComponent
   ],
   imports: [
     CommonModule,
