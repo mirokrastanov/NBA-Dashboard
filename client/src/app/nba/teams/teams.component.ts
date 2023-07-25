84e36b6bb1d861c99bb24243990b28fc023b1b89
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-25 20:47:13 +0300

    Implemented Teams data fetching

--- client/src/app/nba/teams/teams.component.ts
+++ client/src/app/nba/teams/teams.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NbaApiService } from '../nba-api.service';
+import { Team } from '../nba-types';
 
 @Component({
   selector: 'app-teams',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent {
+  constructor(private apiService: NbaApiService) { }
+  teamsArray: Team[] | null = null;
+  metaObject: { [key: string]: any } = {};
+  unsubscribed: boolean = false;
+  errorOccurred: boolean = false;
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    this.apiService.nbaFetch('teams').subscribe({
+      next: (data) => {
+        this.teamsArray = data.data;
+        this.metaObject = data.meta;
+        console.log(data);
+        console.log(data.data[0]);
+        let test: object = {};
+        console.log(test.hasOwnProperty('teams'));
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorOccurred = true;
+        console.log(err);
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.unsubscribed = true;
+        console.log('Subscription ended!');
+      },
+    });
+  }
 
 }
