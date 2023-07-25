8037b594f94da19690b079e07dc8af71173ff2ac
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-25 20:37:20 +0300

    Add extra routes to nba router

--- client/src/app/nba/nba-routing.module.ts
+++ client/src/app/nba/nba-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../core/dashboard/dashboard.component';
 import { StandingsComponent } from './standings/standings.component';
 import { TeamsComponent } from './teams/teams.component';
 import { PlayersComponent } from './players/players.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { NewsComponent } from './news/news.component';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { ErrorComponent } from '../core/error/error.component';
 
 const routes: Routes = [
     {
         path: 'nba',
         children: [
-            { path: '', component: DashboardComponent },
+            { path: '', component: ErrorComponent },
             { path: 'standings', component: StandingsComponent },
             { path: 'teams', component: TeamsComponent },
             { path: 'players', component: PlayersComponent },
+            { path: 'transactions', component: TransactionsComponent },
+            { path: 'news', component: NewsComponent },
+            { path: 'analysis', component: AnalysisComponent },
         ]
     },
 ];
