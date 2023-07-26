dc04129bf358fd4975711e561bcc1dad6ee23492
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-26 18:47:59 +0300

    All leader charts now saving to Firebase

--- web-scrapers/app.js
+++ web-scrapers/app.js
@@ -19,26 +19,28 @@ const db = getDatabase(firebaseApp);
 
 
 async function scrapeAll() {
-    // const standings = await scrapeStandings(scrapeURLs.standings);
-    // const players = await scrapePlayers(scrapeURLs.players);
-    // const news = await scrapeNews(scrapeURLs.news);
-    // const analysis = await scrapeAnalysis(scrapeURLs.analysis);
-    // const transactions = await scrapeTransactions(scrapeURLs.transactions);
+    const standings = await scrapeStandings(scrapeURLs.standings);
+    const players = await scrapePlayers(scrapeURLs.players);
+    const news = await scrapeNews(scrapeURLs.news);
+    const analysis = await scrapeAnalysis(scrapeURLs.analysis);
+    const transactions = await scrapeTransactions(scrapeURLs.transactions);
     const leadersAverages = await scrapeLeaders(scrapeURLs.leaders.averages);
     const leadersTotals = await scrapeLeaders(scrapeURLs.leaders.totals);
     const leadersMisc = await scrapeLeaders(scrapeURLs.leaders.misc);
+    const leadersAdvanced = await scrapeLeaders(scrapeURLs.leaders.advanced);
 
-    // set(ref(db, 'nba/standings'), standings);
-    // set(ref(db, 'nba/players'), players);
-    // set(ref(db, 'nba/news'), news);
-    // set(ref(db, 'nba/analysis'), analysis);
-    // set(ref(db, 'nba/transactions'), transactions);
+    set(ref(db, 'nba/standings'), standings);
+    set(ref(db, 'nba/players'), players);
+    set(ref(db, 'nba/news'), news);
+    set(ref(db, 'nba/analysis'), analysis);
+    set(ref(db, 'nba/transactions'), transactions);
     set(ref(db, 'nba/leaders/averages'), leadersAverages);
     set(ref(db, 'nba/leaders/totals'), leadersTotals);
     set(ref(db, 'nba/leaders/misc'), leadersMisc);
+    set(ref(db, 'nba/leaders/advanced'), leadersAdvanced);
 
     console.log('Saved to Firebase!');
-
+    
     return;
 }
 
