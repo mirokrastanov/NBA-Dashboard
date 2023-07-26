21f6ece3ce4324de1795bd6367f87e300e5b502f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-26 15:31:05 +0300

    NBA News now saving to firebase

--- web-scrapers/app.js
+++ web-scrapers/app.js
@@ -1,12 +1,14 @@
-import { scrapePlayers, scrapeStandings } from "./scrapers.js";
-import { firebaseConfig, scrapeURLs } from './constants.js';
+import { scrapeStandings } from './scrapers/standings.js';
+import { scrapePlayers } from './scrapers/players.js';
 
+import { firebaseConfig, scrapeURLs } from './constants.js';
 import { initializeApp } from 'firebase/app';
 // import {
 //     getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, connectAuthEmulator,
 //     onAuthStateChanged, signOut,
 // } from "firebase/auth";
 import { getDatabase, ref, set, get, child } from 'firebase/database';
+import { scrapeNews } from './scrapers/news.js';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(firebaseApp);
@@ -18,10 +20,12 @@ async function scrapeAll() {
     console.log(standings);
     const players = await scrapePlayers(scrapeURLs.players);
     console.log(players);
+    const news = await scrapeNews(scrapeURLs.news);
+    console.log(news);
 
     set(ref(db, 'nba/standings'), standings);
     set(ref(db, 'nba/players'), players);
-
+    set(ref(db, 'nba/news'), news);
 
     console.log('Saved to Firebase!');
 
