196bd24528dd8fa98acb2a256438e375e90dffd9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-26 16:48:29 +0300

    NBA Analysis data saved to Firebase

--- web-scrapers/app.js
+++ web-scrapers/app.js
@@ -1,5 +1,7 @@
 import { scrapeStandings } from './scrapers/standings.js';
 import { scrapePlayers } from './scrapers/players.js';
+import { scrapeNews } from './scrapers/news.js';
+import { scrapeAnalysis } from './scrapers/analysis.js';
 
 import { firebaseConfig, scrapeURLs } from './constants.js';
 import { initializeApp } from 'firebase/app';
@@ -8,7 +10,6 @@ import { initializeApp } from 'firebase/app';
 //     onAuthStateChanged, signOut,
 // } from "firebase/auth";
 import { getDatabase, ref, set, get, child } from 'firebase/database';
-import { scrapeNews } from './scrapers/news.js';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(firebaseApp);
@@ -22,10 +23,13 @@ async function scrapeAll() {
     console.log(players);
     const news = await scrapeNews(scrapeURLs.news);
     console.log(news);
+    const analysis = await scrapeAnalysis(scrapeURLs.analysis);
+    console.log(analysis);
 
     set(ref(db, 'nba/standings'), standings);
     set(ref(db, 'nba/players'), players);
     set(ref(db, 'nba/news'), news);
+    set(ref(db, 'nba/analysis'), analysis);
 
     console.log('Saved to Firebase!');
 
