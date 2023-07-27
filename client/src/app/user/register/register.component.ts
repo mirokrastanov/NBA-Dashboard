5454133a2caf48b45bb98745d5e09fdfdea6a09f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-27 15:13:11 +0300

    Get data from register form

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
+
+  registerHandler(form: NgForm): void {
+    console.log(form.value);
+  }
 
 }
