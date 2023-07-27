93c0a30630735603f8a9676107e5a822bc8bdef9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-27 18:57:11 +0300

    Get data from login form

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginUser } from 'src/app/util/user-interfaces';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
+
+  loginHandler(form: NgForm): void {
+    const value: LoginUser = form.value;
+    if (form.invalid) {
+      console.log(form.invalid);
+    }
+    // add validator conditionals
+    console.log(form.controls['email'].status);
+    console.log(form.controls['password'].status);
+    
+
+
+  }
 
 }
