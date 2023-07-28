fcdfa1c01c400f6db8b2f729d4f7e16c379412c8
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-28 11:53:52 +0300

    Update other env files

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
+import { defaultEnvironment } from "./environment.default";
+
 export const environment = {
-    firebase: {
-        apiKey: "AIzaSyAoQvUDD11vzDNgv9kuBlnnyDquxvHYQWI",
-        authDomain: "nba-1-480a7.firebaseapp.com",
-        databaseURL: "https://nba-1-480a7-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "nba-1-480a7",
-        storageBucket: "nba-1-480a7.appspot.com",
-        messagingSenderId: "130806097515",
-        appId: "1:130806097515:web:b478223fb8d42565bab32e"
-    },
+    ...defaultEnvironment,
     production: true,
 };
