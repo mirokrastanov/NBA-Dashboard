5fa93c933856289211c27bf0ed2d36885294a0bc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-28 22:05:08 +0300

    Add complete register method with firebase auth

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { RegisterUser } from 'src/app/util/user-interfaces';
+import { AbstractControl, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginUser, RegisterUser } from 'src/app/util/user-interfaces';
+import { AuthService } from '../auth.service';
 
 
 @Component({
@@ -10,20 +12,57 @@ import { RegisterUser } from 'src/app/util/user-interfaces';
 })
 
 export class RegisterComponent {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) { }
+  errorMessage: string | null = null;
+  isLoggingIn: boolean = false;
 
-  registerHandler(form: NgForm): void {
-    const value: RegisterUser = form.value;
-    if (form.invalid) {
-      console.log(form.invalid);
+  onRegister(form: NgForm): void {
+    const formUsername: AbstractControl<any> = form.controls['username'];
+    const formEmail: AbstractControl<any> = form.controls['email'];
+    const formPassword: AbstractControl<any> = form.controls['password'];
+    const formRepeatPassword: AbstractControl<any> = form.controls['repeatPassword'];
+    const params: LoginUser = {
+      email: formEmail.value,
+      password: formPassword.value,
+    };
+    this.isLoggingIn = true;
+    this.errorMessage = null;
+
+    if (params.email == '' || params.password == '' || formUsername.value == ''
+      || formRepeatPassword.value == '') this.errorMessage = 'Inputs cannot be empty';
+    else if (!formEmail.valid) this.errorMessage = 'Invalid Email';
+    else if (!formPassword.valid) this.errorMessage = 'Password must be 6 or more characters';
+    else if (!formRepeatPassword.valid) this.errorMessage = 'Passwords must match';
+
+    if (this.errorMessage != null) {
+      setTimeout(() => { this.errorMessage = null }, 5000);
+      setTimeout(() => { this.isLoggingIn = false }, 1000);
+      return;
     }
-    // add validator conditionals
-    // console.log(form.controls['username'].status);
-    // console.log(form.controls['email'].status);
-    // console.log(form.controls['password'].status);
-    // console.log(form.controls['repeatPassword'].status);
-    
-  }
 
+    this.authService.register(params).subscribe({
+      next: (response) => {
+        this.isLoggingIn = false;
+        this.errorMessage = null;
+        this.authService.authStatusListener();
+        this.authService.updateProfile({
+          displayName: formUsername.value,
+        });
+        form.reset();
+        localStorage.setItem('user', JSON.stringify(response.user));
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.isLoggingIn = false;
+        this.errorMessage = err.message;
+        setTimeout(() => { this.errorMessage = null }, 5000);
+      },
+      complete: () => { }
+    });
+  }
 
 
 }
