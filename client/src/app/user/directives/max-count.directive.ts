44fcc5e820a43050bc9fa7b91590219d781e42fd
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-28 22:47:20 +0300

    Update max count directive

--- client/src/app/user/directives/max-count.directive.ts
+++ client/src/app/user/directives/max-count.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaxCount]'
+  selector: '[appMaxCount]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaxCountDirective,
+      multi: true,
+    },
+  ],
 })
-export class MaxCountDirective {
-
+export class MaxCountDirective implements Validator {
+  @Input() appMaxCount: number | undefined;
   constructor() { }
 
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    // console.log(control, control.value == null);
+
+    if (this.appMaxCount == undefined
+      || control.value?.length <= this.appMaxCount) {
+      return null;
+    }
+
+    return { appMaxCount: this.appMaxCount };
+  }
 }
