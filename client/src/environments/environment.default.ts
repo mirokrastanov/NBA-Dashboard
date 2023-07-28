a9ba81279d7690cfff500527b3368c22a894acfe
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-28 11:53:40 +0300

    Add a default environment file

---
+++ client/src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+export const defaultEnvironment = {
+    firebase: {
+        apiKey: "AIzaSyAoQvUDD11vzDNgv9kuBlnnyDquxvHYQWI",
+        authDomain: "nba-1-480a7.firebaseapp.com",
+        databaseURL: "https://nba-1-480a7-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "nba-1-480a7",
+        storageBucket: "nba-1-480a7.appspot.com",
+        messagingSenderId: "130806097515",
+        appId: "1:130806097515:web:b478223fb8d42565bab32e"
+    },
+    production: false,
+};
