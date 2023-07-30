2918d8959d8fe2c8ac1f53b911f72cbcd79f7409
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-30 18:55:17 +0300

    Add alert and redirect if not authenticated

--- client/src/app/guards/auth.activate.ts
+++ client/src/app/guards/auth.activate.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "../user/auth.service";
 
 
 @Injectable({ providedIn: 'root' })
 export class AuthActivate implements CanActivate {
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(
         route: ActivatedRouteSnapshot,
@@ -15,8 +15,13 @@ export class AuthActivate implements CanActivate {
         | Observable<boolean | UrlTree>
         | Promise<boolean | UrlTree> {
 
-
-        return false;
+        if (this.authService.isAuthenticated) {
+            return true;
+        } else {
+            this.router.navigate(['/user/login']);
+            alert('You must be logged in to view this page!');
+            return false;
+        }
     }
 
 }
