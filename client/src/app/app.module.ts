e5b05f533b51de680835a3eed9c9f49e71f9fd7a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-31 15:26:07 +0300

    Add firebase realtime database connectivity

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { UserModule } from './user/user.module';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { firebaseConfig } from './util/global-constants';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -27,8 +30,10 @@ import { firebaseConfig } from './util/global-constants';
     UserModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
 
-
+    
     AppRoutingModule, // SHOULD REMAIN LAST !!!
   ],
   providers: [],
