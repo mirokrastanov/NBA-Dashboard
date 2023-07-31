0aa159307b0f3654a6157d6819b906e7e7e8738c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-31 13:50:59 +0300

    Create no auth guard for guest only paths

---
+++ client/src/app/guards/noAuth.activate.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../user/auth.service";
+
+
+@Injectable({ providedIn: 'root' })
+export class NoAuthActivate implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    get isAuthenticated(): any {
+        this.authService.authStatusListener();
+        return localStorage.getItem('user');
+    }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot):
+        boolean | UrlTree
+        | Observable<boolean | UrlTree>
+        | Promise<boolean | UrlTree> {
+
+        if (this.isAuthenticated) {
+            this.router.navigate(['/dashboard']);
+            alert('You are already logged in. Redirecting you to the dashboard!');
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
