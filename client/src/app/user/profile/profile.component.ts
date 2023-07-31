e9607cca26da92c2d5ff198defe258d2fc1f9269
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-31 19:48:38 +0300

    Add profile dynamic logic for visualization

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  onFavClick(e: MouseEvent): void {
+    const subTarget: HTMLElement | EventTarget | any = e.target!;
+    if (!subTarget.classList.contains('ques')) return;
+    
+    const target: HTMLElement | EventTarget | any = e.currentTarget!;
+    const ques = this.document.querySelectorAll('.fav');
+
+    if (target.classList.contains('fav-active')) target.classList.remove('fav-active');
+    else {
+      target.classList.add('fav-active');
+      ques.forEach(x => {
+        if (x != target) x.classList.remove('fav-active');
+      })
+    }
+  }
 
 }
