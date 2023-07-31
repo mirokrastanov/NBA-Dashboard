9d649349b75cff7c8c0dc53b45dffb4cfc2e910d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-31 17:18:10 +0300

    Add a test function for later use

--- client/src/app/nba/nba-api.service.ts
+++ client/src/app/nba/nba-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { endpointsNBA, proxy, noProxy } from '../util/global-constants';
 import { EMPTY } from 'rxjs'; // returns an empty observable
+import { Database, set, ref, update } from '@angular/fire/database';
 
 
 @Injectable({
@@ -10,11 +11,18 @@ import { EMPTY } from 'rxjs'; // returns an empty observable
 
 export class NbaApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public database: Database) { }
 
   nbaFetch(target: string = '') { // TODO ==> Add and implement dynamic query params input
     if (!target || !endpointsNBA.hasOwnProperty(target)) return EMPTY;
     let address = endpointsNBA[target];
     return this.http.get<any>(proxy + address);
   }
+
+
+  // testFetch(): void {
+  //   set(ref(this.database, 'users/' + 'test'), {
+  //     credential: 'test-prop'
+  //   });
+  // }
 }
