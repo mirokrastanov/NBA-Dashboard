b7ccd0b85331590072f57a82d7f789d7af75df15
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-02 20:36:43 +0300

    Implement news fetching on init

--- client/src/app/nba/news/news.component.ts
+++ client/src/app/nba/news/news.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NbaApiService } from '../nba-api.service';
+import { News } from '../nba-types';
+import { dbTarget } from 'src/app/util/global-constants';
 
 @Component({
   selector: 'app-news',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent {
+  constructor(private apiService: NbaApiService) { }
+  newsArray: News[] | null = null;
+  unsubscribed: boolean = false;
+  errorOccurred: boolean = false;
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    this.apiService.firebaseDbFetch(dbTarget.nba.news).subscribe({
+      next: (data) => {
+        this.newsArray = data;
+        console.log(this.newsArray);
+        console.log(this.newsArray![0]);
+        
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorOccurred = true;
+        console.log(err);
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.unsubscribed = true;
+        console.log('News data fetched!');
+      },
+    });
+  }
+
+
 
 }
