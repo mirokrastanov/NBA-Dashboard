9346a145386803140b4d36e334b5666d1cc479bc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-03 15:10:48 +0300

    Add data fetching based on route param

--- client/src/app/nba/news-item/news-item.component.ts
+++ client/src/app/nba/news-item/news-item.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NbaApiService } from '../nba-api.service';
+import { News } from '../nba-types';
+import { dbTarget } from 'src/app/util/global-constants';
 
 @Component({
   selector: 'app-news-item',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./news-item.component.css']
 })
 export class NewsItemComponent {
+  constructor(
+    private apiService: NbaApiService,
+    private route: ActivatedRoute,
+  ) { }
+  newsItem: News | null = null;
+  unsubscribed: boolean = false;
+  errorOccurred: boolean = false;
+  isLoading: boolean = true;
+  itemID: number | undefined;
 
+  ngOnInit(): void {
+    this.apiService.firebaseDbFetch(dbTarget.nba.news).subscribe({
+      next: (data) => {
+        this.itemID = this.route.snapshot.params!['id'];
+        this.newsItem = data[this.itemID!];
+        // console.log(this.itemID);
+        // console.log(this.newsItem);
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorOccurred = true;
+        console.log(err);
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.unsubscribed = true;
+        // console.log('News item data fetched!');
+      },
+    });
+  }
 }
