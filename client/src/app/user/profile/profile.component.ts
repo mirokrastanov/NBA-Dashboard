4d1de3bfa58b46631b84c4bf7a9cadb217ff97d4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-03 19:57:36 +0300

    Add multiple functionalities inc data fetching

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
+import { Database } from '@angular/fire/database';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +10,46 @@ import { Component, Inject } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private authService: AuthService,
+    public database: Database,
+  ) { }
+  authProfile: {} | null = null;
+  dbProfile: {} | null = null;
+  unsubscribed: boolean = false;
+  errorOccurred: boolean = false;
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    this.authProfile = JSON.parse(localStorage.getItem('user')!);
+    console.log(this.authProfile);
+
+
+  }
+
+  onUpdateClick(e: MouseEvent): void {
+    // get Update profile from the auth service !!! >> see what fits best
+
+    // this.apiService.firebaseDbFetch().subscribe({
+    //   next: (data) => {
+    //     this.authProfile = data;
+    //     // console.log(this.authProfile);
+
+    //     this.isLoading = false;
+    //   },
+    //   error: (err) => {
+    //     this.errorOccurred = true;
+    //     console.log(err);
+    //     this.isLoading = false;
+    //   },
+    //   complete: () => {
+    //     this.unsubscribed = true;
+    //     // console.log('News item data fetched!');
+    //   },
+    // });
+  }
 
   onFavClick(e: MouseEvent): void {
     const subTarget: HTMLElement | EventTarget | any = e.target!;
@@ -24,5 +66,4 @@ export class ProfileComponent {
       })
     }
   }
-
 }
