84f6f3e80edd7c78502b2ab3c6b58b5797e8545b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-04 16:39:13 +0300

    Create multiple functions for future use

--- client/src/app/nba/players/players.component.ts
+++ client/src/app/nba/players/players.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { NbaApiService } from '../nba-api.service';
 import { Player } from '../nba-types';
+import { dbTarget } from 'src/app/util/global-constants';
 
 @Component({
   selector: 'app-players',
@@ -9,22 +10,23 @@ import { Player } from '../nba-types';
 })
 export class PlayersComponent implements OnInit {
   constructor(private apiService: NbaApiService) { }
-  playersArray: Player[] | null = null;
-  metaObject: { [key: string]: any } = {};
+  playersALL: Player[] | null = null;
+  playersSHOWN: Player[] | null = null;
+  currentPage: number = 1;
+  perPage: number = 25;
   unsubscribed: boolean = false;
   errorOccurred: boolean = false;
   isLoading: boolean = true;
 
   ngOnInit(): void {
-    this.apiService.nbaFetch('players').subscribe({
-      next: (data) => {
-        this.playersArray = data.data;
-        this.metaObject = data.meta;
-        console.log(data);
-        console.log(this.playersArray);
-        console.log(this.metaObject);
-        console.log(data.data[0]);
+    this.apiService.firebaseDbFetch(dbTarget.nba.players).subscribe({
+      next: (data: Player[]) => {
+        this.playersALL = data.slice()
+          .sort((a, b) => a['Player'].localeCompare(b['Player']));
+        // console.log(this.playersALL);
+        // console.log(this.playersALL![0]);
         this.isLoading = false;
+        this.changePage(1);
       },
       error: (err) => {
         this.errorOccurred = true;
@@ -36,8 +38,33 @@ export class PlayersComponent implements OnInit {
         console.log('Players data fetched!');
       },
     });
-    // this.apiService.testFetch();
   }
 
+  onPageBack(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.changePage(this.currentPage);
+    }
+  }
 
+  onPageForward(): void {
+    if (this.currentPage < this.numPages()) {
+      this.currentPage++;
+      this.changePage(this.currentPage);
+    }
+  }
+
+  changePage(page: number): void {
+    if (page < 1) page = 1;
+    if (page > this.numPages()) page = this.numPages();
+    this.playersSHOWN = [];
+    for (let i = (page - 1) * this.perPage; i < (page * this.perPage); i++) {
+      this.playersSHOWN.push(this.playersALL![i]);
+    }
+    console.log(this.playersSHOWN);
+  }
+
+  numPages(): number {
+    return Math.ceil(this.playersALL!.length / this.perPage);
+  }
 }
