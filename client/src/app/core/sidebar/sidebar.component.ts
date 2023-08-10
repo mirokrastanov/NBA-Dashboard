7eb2d1ef46edeaa71f310b21cb9d56e9e4e6b0a4
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-11 01:38:31 +0300

    Update logout. Add loader. Add seq redirect.

--- client/src/app/core/sidebar/sidebar.component.ts
+++ client/src/app/core/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, OnDestroy, Output, OnChanges } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { AuthService } from 'src/app/user/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +9,11 @@ import { AuthService } from 'src/app/user/auth.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit, OnDestroy {
-  constructor(@Inject(DOCUMENT) private document: Document, private authService: AuthService) { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
   aside = () => this.document.body.querySelector('aside')!;
 
   isAuthenticated: any = false;
@@ -16,11 +21,12 @@ export class SidebarComponent implements OnInit, OnDestroy {
     return this.authService.currentUser;
   }
 
+  isLoading = false;
   intervals: any[] = [];
 
   ngOnInit(): void {
     this.authService.fireAuth.authState.subscribe(authStatus => this.isAuthenticated = authStatus);
-    
+
     // console.log(window.innerWidth + 'x' + window.innerHeight);
     let interval = setInterval(() => {
       if (window.innerWidth > 768) {
@@ -41,8 +47,13 @@ export class SidebarComponent implements OnInit, OnDestroy {
   }
 
   onLogout(): void {
+    this.isLoading = true;
     this.authService.logout();
     this.authService.authStatusListener();
     this.isAuthenticated = false;
+    setTimeout(() => {
+      this.isLoading = false;
+      this.router.navigate(['/user/login']);
+    }, 1000);
   }
 }
