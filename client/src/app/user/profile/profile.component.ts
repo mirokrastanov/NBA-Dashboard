786c5fba6e29e87d63e0294cc370f5a45fcd2bcc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-08-10 15:25:24 +0300

    Add various methods/properties and test code

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { NbaApiService } from 'src/app/nba/nba-api.service';
 import { dbTarget } from 'src/app/util/global-constants';
+import { Favorites } from 'src/app/nba/nba-types';
 
 @Component({
   selector: 'app-profile',
@@ -21,46 +22,36 @@ export class ProfileComponent {
   ) { }
   authProfile: any | null = null;
   dbProfile: any | null = null;
+  favorites: Favorites | null = null;
   unsubscribed: boolean = false;
   errorOccurred: boolean = false;
   isLoading: boolean = true;
-  favTeams: number[] = [];
-  favPlayers: number[] = [];
+  favTeams: any[] = [];
+  favPlayers: any[] = [];
 
   ngOnInit(): void {
     // IT WORKS - USE IT when implementing favorites
     // remember to remove the service key during filtering
-    
+
     // const test = this.apiService.testUserExtra(dbTarget.users, JSON.parse(localStorage.getItem('user')!).uid)
     // test.subscribe({
     //   next: (data) => {
     //     console.log(data);
-        
+
     //   }
     // })
-    
+
+
+    // this.apiService.getFavorites().subscribe({
+    //   next: (data) => {
+    //     console.log(data);
+
+    //   }
+    // })
 
     this.authProfile = JSON.parse(localStorage.getItem('user')!);
     // console.log(this.authProfile);
-    this.apiService.firebaseDbFetch(dbTarget.users).subscribe({
-        next: (data) => {
-          Object.entries(data).filter(([key, value], i) => {
-            // console.log(key, value);
-            if (this.authProfile!.uid == key) this.dbProfile = value;
-          });
-          // console.log(this.dbProfile);
-          this.isLoading = false;
-        },
-        error: (err) => {
-          this.errorOccurred = true;
-          console.log(err);
-          this.isLoading = false;
-        },
-        complete: () => {
-          this.unsubscribed = true;
-          // console.log('Data fetched!');
-        },
-      });
+    this.fetchProfile();
 
 
   }
@@ -84,5 +75,50 @@ export class ProfileComponent {
         if (x != target) x.classList.remove('fav-active');
       })
     }
+  }
+
+  fetchProfile(): void {
+    this.apiService.fetchUserExtra().subscribe({
+      next: (data) => {
+        this.dbProfile = data;
+        this.favorites = this.dbProfile.favorites;
+        this.favPlayers = Object.keys(this.favorites!.players).filter(x => x != 'service');
+        this.favTeams = Object.keys(this.favorites!.teams).filter(x => x != 'service');
+        // console.log(this.dbProfile);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.errorOccurred = true;
+        console.log(err);
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.unsubscribed = true;
+        // console.log('Data fetched!');
+      },
+    });
+  }
+
+  onFavPlayer(e: MouseEvent): void {
+    this.apiService.addFavoritePlayer('test1');
+    this.isLoading = true;
+    setTimeout(() => {
+      this.fetchProfile();
+      this.isLoading = false;
+    }, 1000);
+  }
+  onFavTeam(e: MouseEvent): void {
+
+  }
+  onUNfavPlayer(e: MouseEvent): void {
+    this.apiService.removeFavoritePlayer('test1');
+    this.isLoading = true;
+    setTimeout(() => {
+      this.fetchProfile();
+      this.isLoading = false;
+    }, 1000);
+  }
+  onUNfavTeam(e: MouseEvent): void {
+
   }
 }
